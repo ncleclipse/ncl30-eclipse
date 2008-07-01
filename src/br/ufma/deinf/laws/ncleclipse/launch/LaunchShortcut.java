48f7cb672f1b0e245ee34ca2202530668396998d
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2008-07-01 21:22:57 +0000

    *** empty log message ***

--- src/br/ufma/deinf/laws/ncleclipse/launch/LaunchShortcut.java
+++ src/br/ufma/deinf/laws/ncleclipse/launch/LaunchShortcut.java
@@ -1,23 +1,22 @@
 package br.ufma.deinf.laws.ncleclipse.launch;
 
-import java.io.File;
-
+import org.eclipse.core.internal.resources.File;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.ui.ILaunchShortcut;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 
 public class LaunchShortcut implements ILaunchShortcut {
-
+	private String platformPath = Platform.getInstallLocation().getURL().getPath().substring(1);
 	@Override
 	public void launch(ISelection selection, String mode) {
 		// TODO Auto-generated method stub
-		String file = selection.toString();
-		System.out.println(file);
+		String file = ((File)((TreeSelection)selection).getFirstElement()).getLocation().toString();
+		run(file);
 	}
 
 	@Override
@@ -28,14 +27,12 @@ public class LaunchShortcut implements ILaunchShortcut {
 	}
 	
 	public void run(String file){
-		String path = Platform.getInstallLocation().getURL().getPath();
-		String gingaNcl = path.substring(1)+"/plugins/ncl_eclipse_1.0.0/gingancl-java";
-		System.out.println(gingaNcl);
+		String gingaNcl = platformPath+"/plugins/ncl_eclipse_1.0.0/gingancl-java";
 		String cmd [] = new String[1];
 		cmd[0] = "\""+gingaNcl+"/gingancl.bat\" "
 			+"\""+file+"\"";
 		try {
-			Process p = DebugPlugin.exec(cmd, new File(gingaNcl));
+			Process p = DebugPlugin.exec(cmd, new java.io.File(gingaNcl));
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
