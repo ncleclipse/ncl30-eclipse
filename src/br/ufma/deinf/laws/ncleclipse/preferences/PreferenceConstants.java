56f6a371ba24fd343f5323bbde4a843bdc742a23
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2008-11-07 22:02:58 +0000

    *** empty log message ***

---
+++ src/br/ufma/deinf/laws/ncleclipse/preferences/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package br.ufma.deinf.laws.ncleclipse.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_NCL_LAYOUT_EDITOR = "pathPreference";
+	
+}
---
+++ src/br/ufma/deinf/laws/ncleclipse/preferences/ViewsPreferencePage.java
@@ -0,0 +1,53 @@
+package br.ufma.deinf.laws.ncleclipse.preferences;
+
+import org.eclipse.jface.preference.*;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import br.ufma.deinf.laws.ncleclipse.NCLEditorPlugin;
+
+/**
+ * This class represents a preference page that
+ * is contributed to the Preferences dialog. By 
+ * subclassing <samp>FieldEditorPreferencePage</samp>, we
+ * can use the field support built into JFace that allows
+ * us to create a page that is small and knows how to 
+ * save, restore and apply itself.
+ * <p>
+ * This page is used to modify preferences only. They
+ * are stored in the preference store that belongs to
+ * the main plug-in class. That way, preferences can
+ * be accessed directly via the preference store.
+ */
+
+public class ViewsPreferencePage
+	extends FieldEditorPreferencePage
+	implements IWorkbenchPreferencePage {
+
+	public ViewsPreferencePage() {
+		super(GRID);
+		setPreferenceStore(NCLEditorPlugin.getDefault().getPreferenceStore());
+		setDescription("Preferences related to NCL Eclipse Views");
+	}
+	
+	/**
+	 * Creates the field editors. Field editors are abstractions of
+	 * the common GUI blocks needed to manipulate various types
+	 * of preferences. Each field editor knows how to save and
+	 * restore itself.
+	 */
+	public void createFieldEditors() {
+		addField(
+			new BooleanFieldEditor(
+				PreferenceConstants.P_NCL_LAYOUT_EDITOR,
+				"&Open Layout View Editor",
+				getFieldEditorParent()));
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+	
+}
