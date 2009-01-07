5d8f5f7f1ae79b13fbbe25453b82d59664be4e6e
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2009-01-07 04:33:20 +0000

    *** empty log message ***

---
+++ src/br/ufma/deinf/laws/ncleclipse/launch/GingaEmulatorLaunchConfiguration.java
@@ -0,0 +1,18 @@
+package br.ufma.deinf.laws.ncleclipse.launch;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+
+public class GingaEmulatorLaunchConfiguration extends LaunchConfigurationDelegate{
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode,
+			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		System.out.println("Ginga NCL Launch Configuration");
+	}
+
+}
