b6248501bca754549e5f29eb13c5c23328b33804
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2009-07-16 15:33:23 +0000

    Exemplo do TextHover! Mostra "Eu aqui" quando o usuário passa o mouse sobre qualquer parte do texto!

---
+++ src/br/ufma/deinf/laws/ncleclipse/texthover/NCLTextHover.java
@@ -0,0 +1,28 @@
+package br.ufma.deinf.laws.ncleclipse.texthover;
+
+import org.eclipse.jface.text.DefaultTextHover;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.swt.graphics.Point;
+
+public class NCLTextHover extends DefaultTextHover {
+	public NCLTextHover(ISourceViewer sourceViewer) {
+		super(sourceViewer);
+	}
+
+	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion) {
+		if (hoverRegion != null) {
+			return "Eu aqui!";
+		}
+		return "NULL";
+	}
+
+	public IRegion getHoverRegion(ITextViewer textViewer, int offset) {
+		Point selection = textViewer.getSelectedRange();
+		if (selection.x <= offset && offset < selection.x + selection.y)
+			return new Region(selection.x, selection.y);
+		return new Region(offset, 0);
+	}
+}
