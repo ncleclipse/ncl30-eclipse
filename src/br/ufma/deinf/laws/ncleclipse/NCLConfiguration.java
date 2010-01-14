a81b853194c1d0d5b2f5ee19a126126ac7dab7a5
thiago <thiago>
2010-01-14 14:12:54 +0000

    *** empty log message ***

--- src/br/ufma/deinf/laws/ncleclipse/NCLConfiguration.java
+++ src/br/ufma/deinf/laws/ncleclipse/NCLConfiguration.java
@@ -51,7 +51,7 @@ import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 
 import br.ufma.deinf.laws.ncleclipse.format.NCLDocumentFormattingStrategy;
 import br.ufma.deinf.laws.ncleclipse.format.XMLAutoIdentStrategy;
-import br.ufma.deinf.laws.ncleclipse.hover.NCLHoverInformationControl2;
+
 import br.ufma.deinf.laws.ncleclipse.hover.NCLInformationControl2;
 import br.ufma.deinf.laws.ncleclipse.hover.NCLTextHoverExtension2;
 import br.ufma.deinf.laws.ncleclipse.hyperlinks.NCLEclipseHyperlinkDetector;
@@ -268,7 +268,7 @@ public class NCLConfiguration extends TextSourceViewerConfiguration {
 			return new IInformationControlCreator() {
 				public IInformationControl createInformationControl(Shell parent) {
 					// return new DefaultInformationControl(parent); return new
-					return new NCLInformationControl2(parent, false);
+					return new NCLInformationControl2(parent, true);
 				}
 			};
 		} else {
