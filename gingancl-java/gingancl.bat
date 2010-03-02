ceb0c38e35f9d81c522c8412edbb90c6568e5ff0
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2010-03-02 03:01:11 +0000

    *** empty log message ***

--- gingancl-java/gingancl.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set CMPDEPDIR=./lib
-set CMPDEPLIBDIR=%CMPDEPDIR%/ext
-set CMPDEPCOREDIR=%CMPDEPDIR%/core
-
-echo Ginga-NCL Emulator v1.1.1
-echo Copyright PUC-Rio (Laboratorio TeleMidia), 1989-2007
-
-java -cp .;"%CMPDEPLIBDIR%/fobs4jmf.jar";"%CMPDEPLIBDIR%/jmf.jar";"%CMPDEPCOREDIR%/gem.jar";"%CMPDEPCOREDIR%/gingacc-io-impl.jar";"%CMPDEPCOREDIR%/gingacc-io-interface.jar";"%CMPDEPCOREDIR%/gingacc-player-impl.jar";"%CMPDEPCOREDIR%/gingacc-player-interface.jar";"%CMPDEPCOREDIR%/gingancl-adapters-Impl.jar";"%CMPDEPCOREDIR%/gingancl-gui.jar";"%CMPDEPCOREDIR%/gingancl-Impl.jar";"%CMPDEPCOREDIR%/gingancl-Interfaces.jar";"%CMPDEPCOREDIR%/ncl-domImpl.jar";"%CMPDEPCOREDIR%/nclConverter.jar";"%CMPDEPCOREDIR%/nclImplementation.jar";"%CMPDEPCOREDIR%/nclInterface.jar";"%CMPDEPLIBDIR%/cobra.jar";"%CMPDEPLIBDIR%/js.jar";"%CMPDEPLIBDIR%/luajava-1.1.jar"; -Djava.library.path="%CMPDEPLIBDIR%" br.pucrio.telemidia.ginga.ncl.gui.GingaPlayerWindow %1
--- gingancl-java/src/gingancl-interface/br/org/ginga/ncl/adapters/procedural/IProceduralPlayerAdapter.java
+++
@@ -1,8 +0,0 @@
-package br.org.ginga.ncl.adapters.procedural;
-
-import br.org.ginga.ncl.adapters.IFormatterPlayerAdapter;
-import br.org.ginga.ncl.model.event.IFormatterEvent;
-
-public interface IProceduralPlayerAdapter extends IFormatterPlayerAdapter{
-	void setCurrentEvent(IFormatterEvent event);
-}
