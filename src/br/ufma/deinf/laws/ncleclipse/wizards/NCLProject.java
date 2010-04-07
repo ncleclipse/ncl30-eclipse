e7ab449a46f4d6a9f71774847e5dce0748bb1030
rodrim.c <rodrim.c>
2010-04-07 04:59:29 +0000

    Classe para criar o projeto NCL

---
+++ src/br/ufma/deinf/laws/ncleclipse/wizards/NCLProject.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * This file is part of the authoring environment in Nested Context Language -
+ * NCL Eclipse.
+ * 
+ * Copyright: 2007-2009 UFMA/LAWS (Laboratory of Advanced Web Systems), All Rights Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * For further information contact:
+ * 		ncleclipse@laws.deinf.ufma.br
+ * 		http://www.laws.deinf.ufma.br/ncleclipse
+ * 		http://www.laws.deinf.ufma.br
+ ********************************************************************************/
+package br.ufma.deinf.laws.ncleclipse.wizards;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+import java.io.File;
+
+/**
+ * @author Rodrigo Costa <rodrim.c@laws.deinf.ufma.br>
+ * 
+ */
+public class NCLProject extends Wizard implements INewWizard, IExecutableExtension {
+	private WizardNewProjectCreationPage pageOne;
+	private IConfigurationElement configurationElement;
+	/**
+	 * 
+	 */
+	public NCLProject() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		setWindowTitle("NCL Project");
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+		pageOne = new WizardNewProjectCreationPage("NCL Project Wizard");
+		pageOne.setTitle("NCL Project");
+		pageOne.setInitialProjectName("New NCL Project");
+		pageOne.setDescription("Create a NCL Project");
+		addPage(pageOne);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+	
+	    String name = pageOne.getProjectName();
+	    URI location = null;
+	    if (!pageOne.useDefaults()) {
+	        location = new File (pageOne.getLocationURI().getPath() + "/" + name).toURI();
+	    }
+
+	    CustomProjectSupport.createProject(name, location);
+
+	    return true;
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement, java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public void setInitializationData(IConfigurationElement config,
+			String propertyName, Object data) throws CoreException {
+		configurationElement = config;		
+	}
+
+}
