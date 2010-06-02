4786b04fed4eba567d5f24c4bb122ae7e306e079
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2010-06-02 02:32:07 +0000

    First commit about new functionality - Comments (line or selection range).

---
+++ src/br/ufma/deinf/laws/ncleclipse/actions/CommentSelection.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Este arquivo é parte da implementação do ambiente de autoria em Nested 
+ * Context Language - NCL Eclipse.
+ * Direitos Autorais Reservados (c) 2007-2010 UFMA/LAWS (Laboratório de Sistemas 
+ * Avançados da Web)
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob
+ * os termos da Licença Pública Geral GNU versão 2 conforme publicada pela Free 
+ * Software Foundation.
+ *
+ * Este programa é distribuído na expectativa de que seja útil, porém, SEM 
+ * NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral do
+ * GNU versão 2 para mais detalhes. Você deve ter recebido uma cópia da Licença
+ * Pública Geral do GNU versão 2 junto com este programa; se não, escreva para a
+ * Free Software Foundation, Inc., no endereço 59 Temple Street, Suite 330,
+ * Boston, MA 02111-1307 USA.
+ *
+ * Para maiores informações:
+ * - ncleclipse@laws.deinf.ufma.br
+ * - http://www.laws.deinf.ufma.br/ncleclipse
+ * - http://www.laws.deinf.ufma.br
+ *
+ *******************************************************************************
+ * This file is part of the authoring environment in Nested Context Language -
+ * NCL Eclipse.
+ * Copyright: 2007-2010 UFMA/LAWS (Laboratory of Advanced Web Systems), All
+ * Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2 for
+ * more details. You should have received a copy of the GNU General Public 
+ * License version 2 along with this program; if not, write to the Free 
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+ * 02110-1301, USA.
+ *
+ * For further information contact:
+ * - ncleclipse@laws.deinf.ufma.br
+ * - http://www.laws.deinf.ufma.br/ncleclipse
+ * - http://www.laws.deinf.ufma.br
+ *
+ ******************************************************************************/
+package br.ufma.deinf.laws.ncleclipse.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+import br.ufma.deinf.laws.ncleclipse.NCLEditor;
+import br.ufma.deinf.laws.ncleclipse.NCLMultiPageEditor;
+
+/**
+ * @author Roberto Azevedo <roberto@laws.deinf.ufma.br>
+ * 
+ */
+public class CommentSelection extends AbstractHandler {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+		NCLEditor editor = ((NCLMultiPageEditor) page.getActiveEditor())
+				.getNCLEditor();
+		
+		TextSelection selection  = (TextSelection)editor.getSelectionProvider().getSelection();
+		IDocument doc = editor.getInputDocument();
+		
+		
+		//TODO: Check if the line is already commented. If it is uncomment the line
+		if (selection.getLength() == 0){
+			int line = selection.getStartLine();
+			int offset;
+			try {
+				offset = doc.getLineOffset(line);
+				doc.replace(offset, 0, "<!--");
+				offset += doc.getLineLength(line);
+				doc.replace(offset-2, 0, " -->");
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}			
+		}
+		else {
+			//TODO: Comment the range!
+			System.out.println("Comment the range!");
+		}
+		
+		return null;
+	}
+
+}
