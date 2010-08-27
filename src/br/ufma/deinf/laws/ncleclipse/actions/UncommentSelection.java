7c4edb13b21799cacef87c6d1292a114fdff6f08
rodrim.c <rodrim.c>
2010-08-27 16:23:25 +0000

    Atualizando a implementação
    Para descomentar é necessário chamar a action UncommentSelection

--- src/br/ufma/deinf/laws/ncleclipse/actions/UncommentSelection.java
+++ src/br/ufma/deinf/laws/ncleclipse/actions/UncommentSelection.java
@@ -22,11 +22,10 @@
  ********************************************************************************/
 package br.ufma.deinf.laws.ncleclipse.actions;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITypedRegion;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
@@ -39,35 +38,18 @@ import org.eclipse.ui.PlatformUI;
 
 import br.ufma.deinf.laws.ncleclipse.NCLEditor;
 import br.ufma.deinf.laws.ncleclipse.NCLMultiPageEditor;
-import br.ufma.deinf.laws.ncleclipse.document.NCLSourceDocument;
 
 /**
- * @author Rodrigo Costa <rodrim.c@laws.deinf.ufma.br>
- * 
+ * @author Roberto Azevedo <roberto@laws.deinf.ufma.br>
+ *
  */
-public class CommentSelectionAction implements IEditorActionDelegate{
-
-	/**
-	 * 
-	 */
-	public CommentSelectionAction() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
-	 */
-	@Override
-	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		// TODO Auto-generated method stub
-	}
+public class UncommentSelection implements IEditorActionDelegate {
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
 	 */
 	@Override
 	public void run(IAction action) {
-		// TODO Auto-generated method stub
 		IWorkbench wb = PlatformUI.getWorkbench();
 		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
 		IWorkbenchPage page = win.getActivePage();
@@ -80,14 +62,40 @@ public class CommentSelectionAction implements IEditorActionDelegate{
 
 		String commentBegin = "<!--";
 		String commentEnd = "-->";
+		
+		int startLine  = selection.getStartLine();
+		int endLine = selection.getEndLine();
 
 		if (selection.getLength() > 0) {
 			try {
 				int offset;
 				offset = selection.getOffset();
-				doc.replace(offset, 0, commentBegin);
-				offset += selection.getLength() + commentBegin.length();
-				doc.replace(offset, 0, commentEnd);
+				IRegion region = doc.getLineInformationOfOffset(offset);
+				String text = doc.get(doc.getLineOffset(startLine), offset - region.getOffset());
+				
+				int index = text.lastIndexOf(commentBegin);
+				if (index != -1)
+					doc.replace(doc.getLineOffset(startLine) + index, commentBegin.length(), "");
+				else{
+					text = selection.getText();
+					index = text.indexOf(commentBegin);
+					doc.replace(offset + index, commentBegin.length(), "");
+				}
+				
+				offset += selection.getLength();
+				region = doc.getLineInformationOfOffset(offset);
+				text = doc.get(offset, doc.getLineLength(endLine) - (offset - region.getOffset()) );
+				index = text.indexOf(commentEnd);
+				if (index != -1)
+					doc.replace(offset, commentEnd.length(), "");
+				else{
+					text = selection.getText();
+					index = text.lastIndexOf(commentEnd);
+					if (index != -1)
+						doc.replace(selection.getOffset() + index, commentEnd.length(), "");
+				}
+				
+				
 			} catch (BadLocationException e) {
 				e.printStackTrace();
 			}
@@ -109,7 +117,12 @@ public class CommentSelectionAction implements IEditorActionDelegate{
 	}
 
 	/* (non-Javadoc)
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
 	 */
+	@Override
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
