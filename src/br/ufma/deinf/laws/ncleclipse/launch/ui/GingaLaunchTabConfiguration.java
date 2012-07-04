65f700a344d63f2352fedb71582717fdddb62436
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2012-07-04 22:26:22 +0000

    Update Copyright header. Remove the Portuguese version.

--- src/br/ufma/deinf/laws/ncleclipse/launch/ui/GingaLaunchTabConfiguration.java
+++ src/br/ufma/deinf/laws/ncleclipse/launch/ui/GingaLaunchTabConfiguration.java
@@ -1,36 +1,12 @@
 /*******************************************************************************
- * Este arquivo Ã© parte da implementaÃ§Ã£o do ambiente de autoria em Nested 
- * Context Language - NCL Eclipse.
- * Direitos Autorais Reservados (c) 2007-2010 UFMA/LAWS (LaboratÃ³rio de Sistemas 
- * AvanÃ§ados da Web)
+ * This file is part of the NCL authoring environment - NCL Eclipse.
  *
- * Este programa Ã© software livre; vocÃª pode redistribuÃ­-lo e/ou modificÃ¡-lo sob
- * os termos da LicenÃ§a PÃºblica Geral GNU versÃ£o 2 conforme publicada pela Free 
- * Software Foundation.
- *
- * Este programa Ã© distribuÃ­do na expectativa de que seja Ãºtil, porÃ©m, SEM 
- * NENHUMA GARANTIA; nem mesmo a garantia implÃ­cita de COMERCIABILIDADE OU
- * ADEQUAÃ‡ÃƒO A UMA FINALIDADE ESPECÃ�FICA. Consulte a LicenÃ§a PÃºblica Geral do
- * GNU versÃ£o 2 para mais detalhes. VocÃª deve ter recebido uma cÃ³pia da LicenÃ§a
- * PÃºblica Geral do GNU versÃ£o 2 junto com este programa; se nÃ£o, escreva para a
- * Free Software Foundation, Inc., no endereÃ§o 59 Temple Street, Suite 330,
- * Boston, MA 02111-1307 USA.
- *
- * Para maiores informaÃ§Ãµes:
- * - ncleclipse@laws.deinf.ufma.br
- * - http://www.laws.deinf.ufma.br/ncleclipse
- * - http://www.laws.deinf.ufma.br
- *
- *******************************************************************************
- * This file is part of the authoring environment in Nested Context Language -
- * NCL Eclipse.
- * Copyright: 2007-2010 UFMA/LAWS (Laboratory of Advanced Web Systems), All
- * Rights Reserved.
+ * Copyright (C) 2007-2012, LAWS/UFMA.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by
  * the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2 for
--- src/br/ufma/deinf/laws/ncleclipse/wizards/NCLProject.java
+++ src/br/ufma/deinf/laws/ncleclipse/wizards/NCLProject.java
@@ -1,25 +1,26 @@
 /*******************************************************************************
- * This file is part of the authoring environment in Nested Context Language -
- * NCL Eclipse.
- * 
- * Copyright: 2007-2009 UFMA/LAWS (Laboratory of Advanced Web Systems), All Rights Reserved.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
+ * This file is part of the NCL authoring environment - NCL Eclipse.
+ *
+ * Copyright (C) 2007-2012, LAWS/UFMA.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by
  * the Free Software Foundation.
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
- * PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more 
- * details.
- * 
- * You should have received a copy of the GNU General Public License version 2
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2 for
+ * more details. You should have received a copy of the GNU General Public 
+ * License version 2 along with this program; if not, write to the Free 
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+ * 02110-1301, USA.
+ *
  * For further information contact:
- * 		ncleclipse@laws.deinf.ufma.br
- * 		http://www.laws.deinf.ufma.br/ncleclipse
- * 		http://www.laws.deinf.ufma.br
- ********************************************************************************/
+ * - ncleclipse@laws.deinf.ufma.br
+ * - http://www.laws.deinf.ufma.br/ncleclipse
+ * - http://www.laws.deinf.ufma.br
+ *
+ ******************************************************************************/
 package br.ufma.deinf.laws.ncleclipse.wizards;
 
 import java.io.File;
@@ -104,6 +105,5 @@ public class NCLProject extends Wizard implements INewWizard, IExecutableExtensi
 		configurationElement = config;		
 	}
 	
-
 
 }
